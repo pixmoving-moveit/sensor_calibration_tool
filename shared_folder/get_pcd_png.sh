5d2ea20d289a7a4cdd3988bc5fb869cd112e8508
zymouse <mouse_zy@qq.com>
2023-04-10 11:51:50 +0800

    fix:Get pcd and pnd files from topics

--- shared_folder/get_pcd_png.sh
+++ shared_folder/get_pcd_png.sh
@@ -10,18 +10,22 @@ export png_topic_name_env=$(yq e  '.get_cail_data.png_topic_name' $config_file_p
 export pcd_topic_name_env=$(yq e  '.get_cail_data.pcd_topic_name' $config_file_path)
 # export pcd_file_path_env
 
+png_path=""
+pcd_path=""
+
 function creat_folder(){
     log_info "create calibration save data folder"
 
     timestamp=$(date "+%Y-%m-%d_%H-%M-%S")
-
-    pcd_path="/root/shared_folder/pix_data/$timestamp/pcd"
-    mkdir -p $pcd_path
-    export pcd_file_path_env=pcd_path
+    index=$(date "%M%S")
 
     png_path="/root/shared_folder/pix_data/$timestamp/png"
     mkdir -p $png_path
-    export png_file_path_env=png_path
+    export png_file_path_env="$png_path/%02d.png"
+
+    pcd_path="/root/shared_folder/pix_data/$timestamp/pcd"
+    mkdir -p $pcd_path
+    export pcd_file_path_env="$pcd_path/"
 }
 
 function run_roslaunch(){
@@ -29,10 +33,20 @@ function run_roslaunch(){
     source /root/sensor_driver_ws/devel/setup.bash 
     roslaunch /root/shared_folder/get_cail_data/get_pcd_png.launch
 }
+function rename_pcd(){
+    i=0
+    for file in $pcd_path/*
+    do
+    mv "$file" "$(printf $pcd_path/'%02d.pcd' $i)"
+    i=$((i+1))
+    done
+}
 
 function main(){
     creat_folder
     run_roslaunch
+    rename_pcd
+    # log_info "$pcd_file_path_env"
 }
 main
 
