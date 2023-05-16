a16235b7256199e37d8abe4a4d55796e16cbb736
zymouse <46778435+zymouse@users.noreply.github.com>
2023-05-16 14:51:53 +0800

    fix:Update camera_intrinsic.sh

--- calibration_script/camera_intrinsic/camera_intrinsic.sh
+++ calibration_script/camera_intrinsic/camera_intrinsic.sh
@@ -4,10 +4,15 @@ source $SCRIPT_DIR/../common.sh
 source $SCRIPT_DIR/../../common_script/log.sh
 
 function move_cali_file() {
-    path=/tmp/calibrationdata.tar.gz
+    path1="$HOME/Downloads/"
+    sudo cp /tmp/calibrationdata.tar.gz $path1/
+    path="$path1/calibrationdata.tar.gz"
+    sudo chmod 666 $path
     if [ -e "$path" ]; then
-        tar -xzf $path -C /tmp/
-        mv /tmp/calibrationdata/ost.yaml  "$SCRIPT_DIR/ost.yaml"
+        rm -rf $path1/calibrationdata
+        mkdir -p $path1/calibrationdata
+        tar -xzf $path -C "$path1/calibrationdata"
+        mv "$path1/calibrationdata/ost.yaml"  "$SCRIPT_DIR/output/ost.yaml"
         $SCRIPT_DIR/parser.py
         log_info "Calibration successful, output [$SCRIPT_DIR/output/center_camera-intrinsic.json]"
     else
@@ -17,8 +22,8 @@ function move_cali_file() {
 
 
 function main(){
-    source $SCRIPT_DIR/../../../sensors_calibration_tool/calibration_ws/install/setup.bash
-    ros2 run camera_calibration cameracalibrator --size 11x8 --square 0.5 --ros-args -r image:=/image_raw -p camera:=/camera
+    source $SCRIPT_DIR/../../sensors_calibration_tool/calibration_ws/install/setup.bash
+    ros2 run camera_calibration cameracalibrator --size 11x8 --square 0.5 --ros-args -r image:=/camera/image_raw -p camera:=/camera
 
     stop 2
     move_cali_file
